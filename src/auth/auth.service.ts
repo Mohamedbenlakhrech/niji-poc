ac77bf15f3e5c966697f227ebbc770ac77ac122e
Mohamed BEN LAKHRECH <mohamed.ben-lakhrech@niji.fr>
2020-08-28 10:26:38 +0200

    fix duplicate users on signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/users/schemas/user.schema';
 import { Model } from 'mongoose';
@@ -7,14 +7,25 @@ import { AuthCredentialsDto } from './dto/authCrednetialsDto.dto';
 
 @Injectable()
 export class AuthService {
+    
     constructor(@InjectModel(User.name) private userModel: Model <User>) {}
 
     async signup(authCredentialsDto: AuthCredentialsDto): Promise<User> {
         const { username, password } = authCredentialsDto;
+
+        const exits = await this.userModel.findOne({ username });
+        if(exits) {
+            throw new ConflictException('Username already exists');
+        }
+
         const user = new this.userModel(authCredentialsDto);
         user.username = username;
-        user.password = password;
-        await user.save();
-        return user;
+        user.password = password;   
+        try {
+            await user.save();
+            return user;
+        } catch(error) {
+            throw new InternalServerErrorException();
+        }
     }
 }
