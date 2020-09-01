393f999a682aca27492533e7803ed64a18732dc7
Mohamed BEN LAKHRECH <mohamed.ben-lakhrech@niji.fr>
2020-09-01 13:56:03 +0200

    Signin OK

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,4 +10,9 @@ export class AuthController {
     signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
         return this.authservice.signup(authCredentialsDto);
     }
+
+    @Post("/signin")
+    signin(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+        return this.authservice.validatePassword(authCredentialsDto)
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, ConflictException, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, ConflictException, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { Model } from 'mongoose';
-import { CreateUserDTO } from 'src/users/dto/create-user.dto';
+
 import { AuthCredentialsDto } from './dto/authCrednetialsDto.dto';
 import { User } from 'src/users/schemas/user.schema';
 
@@ -32,7 +32,35 @@ export class AuthService {
         }
     }
 
+    /*async signin(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+        const { username, password } = authCredentialsDto;
+
+        const exists = await this.userModel.findOne({ username });
+        if(exists)
+    }*/
+
+    // hash password for signup
     private async hashPassword(password: string, salt: string): Promise<string> {
         return bcrypt.hash(password, salt);
+    }
+
+    // validate password for signin
+    async validatePassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userModel.findOne({ username });
+
+        if(user) {
+            // compare password
+            const validPassword = await bcrypt.compare(password, user.password);
+            console.log(`Plain pass ${password} | User pass ${user.password}`)
+            console.log(validPassword)
+            if(validPassword) {
+                return username;
+            } else {
+                throw new UnauthorizedException('Invalid credentials');
+            }            
+        } else {
+            throw new NotFoundException('User not found');
+        }
     }
 }
